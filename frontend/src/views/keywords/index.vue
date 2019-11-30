e04ffa5498f45d3294953315786a3d0487dcf585
Temaxu <1933025651@qq.com>
2019-11-30 17:09:33 +0800

    分词跨域
    分词测试

--- frontend/src/views/keywords/index.vue
+++ frontend/src/views/keywords/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    这是关键词
+    <div>
+      分词页面
+      <input v-model='source' placeholder="化核加应子一箱24公斤湿乌梅子蜜饯果脯凉果散装批发潮汕特产"/>
+      <button @click="get">提交</button>
+      <div>{{ value }}</div>
+    </div>
   </div>
 </template>
 
@@ -8,13 +13,37 @@
 export default {
     data() {
         return {
-
+            source: '化核加应子一箱24公斤湿乌梅子蜜饯果脯凉果散装批发潮汕特产',
+            value: [] // { "t": "化核", "p": "0.945319" }, { "t": "加应子", "p": "0.87609" },
         }
     },
     created() {
-        let title = this.$route.query.title
-        console.log(title)
-        alert(title)
+        // let title = this.$route.query.title
+        // console.log(title)
+        // alert(title)
+    },
+    methods: {
+        get() {
+             this.$axios({
+                method: 'get',
+                url: 'http://120.77.183.17/analyizer',
+                params: {
+                    source: this.source,
+                    param1: 0,
+                    param2: 1,
+                    json: 1
+                }}).then(res => {
+                console.log(res)
+                alert('登陆成功')
+                // 将获取的分词通过p 参数的大小进行反向排序
+                res.data.sort(function(a, b) {
+                    a.p = parseFloat(a.p)
+                    b.p = parseFloat(b.p)
+                    return b.p - a.p
+                })
+                this.value = res
+            })
+        }
     }
 }
 </script>
