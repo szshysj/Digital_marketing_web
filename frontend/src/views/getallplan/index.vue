89b89505df4ee179cf8fc73f63ac960dcbb3ef0e
大东东 <1195902401@qq.com>
2019-12-07 10:13:21 +0800

    完善

--- frontend/src/views/getallplan/index.vue
+++ frontend/src/views/getallplan/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-container">
     <!-- header -->
-    <div>
+    <div class="filter-container">
       <el-input v-model="listQuery.title" placeholder="Title" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
       <!-- <el-select v-model="listQuery.importance" placeholder="Imp" clearable style="width: 90px" class="filter-item">
         <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item" />
